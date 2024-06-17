45269ddccecef52ebadbb8f76ce257bf8217b036
CosmoCat1111 <131853637+CosmoCat1111@users.noreply.github.com>
2024-06-17 00:48:54 -0700

    Add project files.

---
+++ MainMethodAssignment/MathOperations.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+
+    public class MathOperations
+    {
+        // Method 1 -- addition
+        public int MathOp(int number)
+        {
+            int result = number + 10;
+            return result;
+        }
+
+        // Method 2 - multiply
+        public int MathOp(decimal number)
+        {
+            int result = (int)(number * 2);
+            return result;
+        }
+
+        // Method 3 - division 
+        public int MathOP(string input)
+        {
+            if (int.TryParse(input, out int number))
+            {
+                int result = number / 5;
+                return result;
+            }
+            else
+            {
+                throw new ArgumentException("Not valid, please enter an integer:");
+                Console.ReadLine();
+            }
+
+        }
+
+    }
+
+}
---
+++ MainMethodAssignment/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+   class Program
+    {
+        static void Main(string[] args)
+        {
+            // calls class
+            MathOperations math = new MathOperations();
+
+            // method 1  - integer
+            int integerResult = math.MathOp(15);
+            Console.WriteLine("Result of integer operation: " + integerResult);
+            Console.ReadLine();
+
+            //method 2 - decimal
+            decimal decimalValue = 7.5m;
+            int decimalResult = math.MathOp(decimalValue);
+            Console.WriteLine("Result of decimal operation: " + decimalResult);
+            Console.ReadLine();
+
+            // method 3 - string
+            string inputString = "50";
+            int stringResult = Convert.ToInt32(inputString);
+            Console.WriteLine("Result of string Operation is " + stringResult);
+            Console.ReadLine();
+
+            //exception handling
+            string invalidInput = "abc";
+            try
+            {
+                string invalidResult = Convert.ToString(invalidInput);
+                Console.WriteLine("Invalid String results are " + invalidInput);
+                Console.ReadLine();
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("Error, please enter integer:");
+                //Console.ReadLine();
+            }
+            Console.ReadLine();
+        }
+    }
+
+}
